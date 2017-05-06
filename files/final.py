4a85bed26367fb62544f9d5ce1b60fe54131a8fc
arvigj <ag4571@nyu.edu>
2017-05-06 19:56:55 -0400

    finally

--- files/final.py
+++ files/final.py
@@ -23,38 +23,38 @@ d2r = sympy.solve(w(2),sympy.Derivative(r(tau),tau,tau))[0].subs({sympy.Derivati
 d2theta = sympy.solve(w(3),sympy.Derivative(theta(tau),tau,tau))[0].subs({sympy.Derivative(t(tau),tau):a,sympy.Derivative(r(tau),tau):b,sympy.Derivative(phi(tau),tau):c,sympy.Derivative(theta(tau),tau):d})
 d2phi = sympy.solve(w(4),sympy.Derivative(phi(tau),tau,tau))[0].subs({sympy.Derivative(t(tau),tau):a,sympy.Derivative(r(tau),tau):b,sympy.Derivative(phi(tau),tau):c,sympy.Derivative(theta(tau),tau):d})
 
-#d2tf = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2t,modules="numpy")
-#d2rf = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2r,modules="numpy")
-#d2thetaf = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2theta,modules="numpy")
-#d2phif = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2phi,modules="numpy")
-#
-#
-#def geo(x, tau):
-#    '''
-#    x[0] = t
-#    x[1] = r
-#    x[2] = phi
-#    x[3] = theta
-#    x[4] = dt dtau
-#    x[5] = dr dtau
-#    x[6] = dtheta dtau
-#    x[7] = dphi dtau
-#    '''
-#    dxdtau = np.zeros_like(x)
-#
-#    dxdtau[0] = x[4]
-#    dxdtau[1] = x[5]
-#    dxdtau[2] = x[6]
-#    dxdtau[3] = x[7]
-#    dxdtau[4] = d2tf(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
-#    dxdtau[5] = d2rf(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
-#    dxdtau[6] = d2thetaf(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
-#    dxdtau[7] = d2phif(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
-#    return dxdtau
-#
-#tau = np.arange(0,10,.1)
-#x0_ = np.array([1,2,0,4,5,6,0,6])
-#x_ = odeint(geo, x0_, tau)
-#
-#print x_
-#
+d2tf = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2t,modules="numpy")
+d2rf = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2r,modules="numpy")
+d2thetaf = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2theta,modules="numpy")
+d2phif = lambdify((a,b,c,d,t(tau),r(tau),theta(tau),phi(tau)),d2phi,modules="numpy")
+
+
+def geo(x, tau):
+    '''
+    x[0] = t
+    x[1] = r
+    x[2] = phi
+    x[3] = theta
+    x[4] = dt dtau
+    x[5] = dr dtau
+    x[6] = dtheta dtau
+    x[7] = dphi dtau
+    '''
+    dxdtau = np.zeros_like(x)
+
+    dxdtau[0] = x[4]
+    dxdtau[1] = x[5]
+    dxdtau[2] = x[6]
+    dxdtau[3] = x[7]
+    dxdtau[4] = d2tf(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
+    dxdtau[5] = d2rf(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
+    dxdtau[6] = d2thetaf(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
+    dxdtau[7] = d2phif(x[0],x[1],x[2],x[3],x[4],x[5],x[6],x[7])
+    return dxdtau
+
+tau = np.arange(0,10,.1)
+x0_ = np.array([1,2,0,4,5,6,0,6])
+x_ = odeint(geo, x0_, tau)
+
+print x_
+
